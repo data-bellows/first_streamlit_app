707bbdb46ddb2a4be706fceef2f824de83da4502
data-bellows <100886730+data-bellows@users.noreply.github.com>
2022-05-25 22:22:39 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-
-streamlit.write('The user entered', fruit_choice)
+# streamlit.write('The user entered', fruit_choice)
 
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
