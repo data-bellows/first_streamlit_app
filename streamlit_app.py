985d7ec52dfd55986b15361cb9921ebdab66b6e0
data-bellows <100886730+data-bellows@users.noreply.github.com>
2022-05-25 22:00:43 +1000

    Update streamlit_app.py
    Add an attempt to connect to snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -35,3 +34,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
+
+
+
